a410d8cd25146e1358b4303499394f5e939d42ca
Maxplaytvbr <maxtvservidoroficial@gmail.com>
2025-06-21 20:49:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const API_KEY = '54b9cff0f48a3f127fa5cd5906bbe251';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+let currentMovie = null;
+let currentLayout = 1;
+let logoImage = null;
+let whatsappNumber = "(11) 99999-9999";
+let whatsappText = "Assine agora";
+
+// DOM Elements
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+// ... (todo o restante do seu JavaScript)
